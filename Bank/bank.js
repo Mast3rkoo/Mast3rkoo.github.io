5f94580b3b41d074b86a4ac8b00deff0dec7d5a1
Filip Noga <fnoga443@gmail.com>
2023-06-07 22:14:57 +0300

    Trying to code up the transaction div

--- Bank/bank.js
+++ Bank/bank.js
@@ -1,11 +1,13 @@
 import { Noga, Brenkusova, Nogova } from "../CreateBankAccount/createBank.js"
 
+// GETTING THE LOGGED ACCOUNT INFO
 const loggedInAccount = JSON.parse(localStorage.getItem('loggedInAccount'));
 const loggedInSavingAccount = JSON.parse(localStorage.getItem('loggedInSavingAccount'));
+
+// GOAL SAVING BALANCE CIRCLE
 const circle = document.querySelector('circle');
 const savingCircleNumber = document.getElementById("number");
 const savingCircleRange = document.getElementById('range')
-
 const savedMoney = 2000;
 const currentSavings = savedMoney  / loggedInSavingAccount.goalBalance * 100;  
 const circleFiller = 450 - currentSavings / 100 * 450;
@@ -21,6 +23,7 @@ setInterval(() => {
     }
 }, 25);
 
+// USER NAME AND USER BALANCE SHOWER
 document.getElementById('accountBalance').textContent = loggedInAccount.balance + " €";
 
 document.getElementById('user-name').textContent = loggedInAccount.surname
@@ -43,3 +46,13 @@ function addRow(newTransaction) {
 }
   
 addRow(newTransactionRow);
+
+// NEW TRANSACTION OPERATIONS 
+
+const transactionSubmitButton = document.getElementById("transaction-button")
+
+transactionSubmitButton.onclick = () => {
+  const transactionName = document.getElementById('transaction-name').innerText;
+  const transactionValue = document.getElementById('transaction-amount').innerText;
+  console.log(loggedInAccount)
+}
